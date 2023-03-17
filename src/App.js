6dfbb46105cd82939666eb7257548b60e7496127
tusharb <tusharbansal@iitbhilai.ac.in>
2023-03-18 02:42:28 +0530

    navbar and header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/header.jsx' ;
+import Navbar from './components/navbar.jsx';
+import Hero from './components/hero.jsx';
 
 function App() {
   return (
     <div>
       <Header />
+      <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/hero.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles/hero.css";
+
+function Hero(){
+   return <div className="main-container">
+    </div>
+}
+
+export default Hero;
